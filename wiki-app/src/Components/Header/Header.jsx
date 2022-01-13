9ec8e2086a430b18ea6035035cb8ea86263dcf2b
Mkisecok <mahmutkisecok@gmail.com>
2022-01-13 11:08:55 +0100

    Add list item navlink in header

--- wiki-app/src/Components/Header/Header.jsx
+++ wiki-app/src/Components/Header/Header.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React,{ useState, useEffect } from 'react'
+import { NavLink, Link } from 'react-router-dom'
 
 function Header() {
+
+    const [ randomEntry, setRandomEntry ] = useState('');
+
+    useEffect(() => {
+            //wir holen uns alle einträge aus der localstorage
+            
+            const allEntries = JSON.parse(window.localStorage.getItem('entries'))
+
+            //wir führen das zufällige  selectieren eines eintrags nur aus, wenn der wert entries in der localstorage existiert, und mindestens einen eintrag hat 
+
+            if(allEntries && allEntries.length > 0 )
+            {
+                const id = allEntries[Math.floor(Math.random() * allEntries.length)].id
+
+                setRandomEntry(id)
+            }
+        
+    }, [])
     return (
         <div className='Header'> 
-            header
+            <ul>
+                <li> <NavLink to='/'>StartSeite</NavLink></li>
+                <li> <NavLink to={`/entry/${ randomEntry }`}>Zufälliger Eintrag </NavLink></li>
+                <li> <NavLink to='/entry/create'>Eintrag Erstellen</NavLink></li>
+            </ul>
         </div>
     )
 }
