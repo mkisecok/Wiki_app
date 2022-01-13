9837d8913fce181ca233d80f964825b1a32b0c0b
Mkisecok <mahmutkisecok@gmail.com>
2022-01-13 15:54:32 +0100

    Add Link to welcome page

--- wiki-app/src/Pages/Welcome.jsx
+++ wiki-app/src/Pages/Welcome.jsx
@@ -8,7 +8,7 @@ function Welcome() {
     const[ loading, setLoading ] = useState(false);
 
     useEffect(()=>{
-        const allEntries= JSON.parse(window.localStorage.getItem/'entries') || []
+        const allEntries= JSON.parse(window.localStorage.getItem('entries'))||[]
 
         const sortedEntries = allEntries.reduce((r,e)=>{
 
@@ -22,12 +22,59 @@ function Welcome() {
             {
                 r[group].children.push(e)
             }
-        })
+            r[group].children.sort((a,b)=>a.title.toUpperCase() > b.title.toUpperCase() )
+           
+            return r
+        }, {})
+
+        const result=Object.values(sortedEntries)
+        result.sort((a,b)=> a.group.toUpperCase() > b.group.toUpperCase() )
+        setEntries(result);
+        setLoading(false)
     },[])
 
     return (
         <div className='Welcome'>
-            welcome
+            <h2> Willkommen auf unserer Wiki </h2>
+
+            {
+                loading
+                ?
+                <p>Loading...</p>
+                :
+                <>
+                <h3>Einträge</h3>
+                {
+                    entries.length === 0 
+                    ?
+                    <p>Keine Einträge vorhanden</p>
+                    :
+                    entries.map((item,i)=>
+                    {
+                        return(
+                            <div key={ i }>
+                                <strong>{item.group}</strong>
+                                <br />
+                                <ul>
+                                    {
+                                        item.children.map((entry,j)=>
+                                        {
+                                            return(
+                                                <li key={ j }>
+                                                    <Link to={`/entry/${entry.id}`}> {entry.title}</Link>
+                                                </li>
+                                            )
+                                        })
+                                    }
+                                </ul>
+                            </div>
+
+                        )
+                    })
+
+                }
+                </>
+            }
         </div>
     )
 }
