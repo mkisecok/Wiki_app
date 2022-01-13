899c54d760b0fd5c7ae42f4cd5675f1a291cb8d1
Mkisecok <mahmutkisecok@gmail.com>
2022-01-13 11:22:32 +0100

    Add NotFound Pages

--- wiki-app/src/Pages/NotFound.jsx
+++ wiki-app/src/Pages/NotFound.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
-
+import { useNavigate } from 'react-router-dom'
 function NotFound() {
+
+    const navigate=useNavigate();
+
+
     return (
         <div className='NotFound'>
-            not found
+            <h1>404</h1>
+
+            <p>
+                Seite nicht gefunden
+            </p>
+
+            <button onClick={()=> navigate(-1) }>Zurück zur vorherigen Seite</button>
         </div>
     )
 }
