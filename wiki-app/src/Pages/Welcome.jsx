d7e69954fb1a75b63af529a0bcd5f5196536b50a
Mkisecok <mahmutkisecok@gmail.com>
2022-01-17 15:44:20 +0100

    styled: Add semantic ui in welcome page

--- wiki-app/src/Pages/Welcome.jsx
+++ wiki-app/src/Pages/Welcome.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-
+import { Container, Divider, Popup } from 'semantic-ui-react'
 
 function Welcome() {
 
@@ -34,9 +34,10 @@ function Welcome() {
     },[])
 
     return (
+        <Container textAlign='left' style={{margin:'20px'}}>
         <div className='Welcome'>
-            <h2> Willkommen auf unserer Wiki </h2>
-
+            <h2 > Willkommen auf unserer Wiki </h2>
+            
             {
                 loading
                 ?
@@ -54,6 +55,7 @@ function Welcome() {
                         return(
                             <div key={ i }>
                                 <strong>{item.group}</strong>
+                                
                                 <br />
                                 <ul>
                                     {
@@ -61,12 +63,21 @@ function Welcome() {
                                         {
                                             return(
                                                 <li key={ j }>
-                                                    <Link to={`/entry/${entry.id}`}> {entry.title}</Link>
+                                                    <Popup
+                                                    content={`${entry.content.slice(0,50)}...`}
+                                                    trigger={<Link to={`/entry/${entry.id}`}> {entry.title}</Link>}
+                                                    position='right center'
+                                                    offset={[0, 50]}
+                                                    inverted
+                                                    />
+                                                    
+                                                    
                                                 </li>
                                             )
                                         })
                                     }
                                 </ul>
+                                <Divider/>
                             </div>
 
                         )
@@ -75,7 +86,9 @@ function Welcome() {
                 }
                 </>
             }
+            
         </div>
+        </Container>
     )
 }
 
